0899c00c8b96ff9a86a89bb9496a9e75a3755e10
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-09-27 21:15:32 +0800

    0.0.1

--- README.md
+++ README.md
@@ -11,15 +11,21 @@ music21与musicpy对象互转
 |-|-|-|
 |music21.pitch.Pitch|musicpy.note||
 |music21.note.Note|musicpy.note|musicpy不支持歌词|
+|music21.tempo.MetronomeMark|musicpy.tempo||
 |music21.stream.Stream|musicpy.track||
+|music21.stream.Measure|musicpy.track||
+|music21.stream.Part|musicpy.track||
+|music21.stream.Score|musicpy.piece|musicpy不支持节拍|
 
 ### `music21py.mpy_to_m21`：musicpy转music21
 支持的类型：
 |转换前的类型|转换后的类型|说明|
 |-|-|-|
 |musicpy.note|music21.note.Note||
+|musicpy.tempo|music21.tempo.MetronomeMark||
 |musicpy.chord|music21.stream.Stream||
-|musicpy.track|music21.stream.Stream||
+|musicpy.track|music21.stream.Part||
+|musicpy.piece|music21.stream.Score||
 
 ## 示例
 ```
@@ -34,8 +40,8 @@ strm.show()
 ## 为什么要开发这个库
 在我看来，musicpy和music21是两个口味不同的Python乐理分析库。
 
-music21偏“学院派”，它的数据结构对标五线谱显示。音高可以用德语、意大利语、盲文等方式显示与输入，时值也支持eighth、16th等字符串形式，支持以基本时值+附点的方式展示与输入。music21严格区分等音，例如`C#`、`D-`、`E---`。music21“大而全”，支持多种文件输入输出。在编写与music21集成的程序时，容易陷入困惑：我不知道隐藏在我所操作的变量下面有多少信息量。
+music21偏“学院派”，它的数据结构对标五线谱显示。音高可以用德语、意大利语、盲文等方式显示与输入，时值也支持eighth、16th等字符串形式，支持以基本时值+附点的方式展示与输入。music21严格区分等音，例如`C#`、`D-`、`E---`。music21“大而全”，支持musicxml、abc等多种文件输入输出。在编写与music21集成的程序时，容易陷入困惑：我不知道隐藏在我所操作的变量下面有多少信息量。
 
-而musicpy更符合现代电子音乐人的口味，其基本数据结构“小而精”。例如，用同一种数据结构来表示和弦与旋律段落。musicpy支持常用的运算符号，使代码大大缩短，开发效率更高。
+而musicpy更符合现代电子音乐人的口味，其基本数据结构“小而精”。例如，用同一种数据结构来表示和弦与旋律段落。musicpy支持常用的运算符号，常用的函数都有简称，使代码大大缩短，开发效率更高（但可维护性变差，如同shell脚本）。
 
-在使用music21时，我绝大多数时间都在与最基本的数据结构打交道，偶尔需要使用高级功能。因此，可以用musicpy完成绝大多数操作，在用到高级功能时再使用本库转换为music21对象
+在使用music21时，我绝大多数时间都在与最基本的数据结构打交道，但偶尔也需要使用高级功能。因此，可以用musicpy完成绝大多数操作，在用到高级功能时再使用本库转换为music21对象
--- music21py/__init__.py
+++ music21py/__init__.py
@@ -1,3 +1,5 @@
+__version__=="0.0.1"
+
 import music21
 import musicpy
 
@@ -23,6 +25,10 @@ def m21_note_Note_to_mpy(m21note:music21.note.Note)->musicpy.note:
     mpynote.duration=m21note.quarterLength/4
     return mpynote
 
+def m21_tempo_MetronomeMark_to_mpy(m21metro:music21.tempo.MetronomeMark)->musicpy.tempo:
+    """将musicpy曲速对象转换为music21曲速对象"""
+    return musicpy.tempo(m21metro.number)
+
 def m21_stream_Stream_to_mpy(m21stream:music21.stream.Stream)->musicpy.track:
     """将music21 Stream转换为musicpy track"""
     #这里使用track而不是chord，因为chord不支持开头休止符的情况
@@ -35,34 +41,70 @@ def m21_stream_Stream_to_mpy(m21stream:music21.stream.Stream)->musicpy.track:
         elif(type(m21obj)==music21.note.Rest):
             mpyinterval[-1]+=m21obj.quarterLength/4
     mpychord=musicpy.chord(mpynotes,interval=mpyinterval[1:])
-    return musicpy.track(content=mpychord,start_time=mpyinterval[0])
+    #如果是part类型，则把m21stream.partName作为musicpy.track的track_name
+    return musicpy.track(content=mpychord,start_time=mpyinterval[0],track_name=getattr(m21stream,'partName',None))
+
+def m21_stream_Score_to_mpy(m21score:music21.stream.Score)->musicpy.piece:
+    """将music21多轨工程转换为musicpy多轨工程"""
+    #print([m21_stream_Stream_to_mpy(m21part) for m21part in m21score.parts])
+    return musicpy.build(*[m21_stream_Stream_to_mpy(m21part) for m21part in m21score.parts])
+    #TODO:曲速
 
 def m21_to_mpy(m21obj):
+    """将music21对象转换为musicpy对象"""
     return {
         music21.pitch.Pitch:m21_pitch_Pitch_to_mpy,
         music21.note.Note:m21_note_Note_to_mpy,
         music21.stream.Stream:m21_stream_Stream_to_mpy,
+        music21.stream.Measure:m21_stream_Stream_to_mpy,
+        music21.stream.Part:m21_stream_Stream_to_mpy,
+        music21.stream.Score:m21_stream_Score_to_mpy,
         }[type(m21obj)](m21obj)
 
 #===================musicpy转music21===================
 
 def mpy_note_to_m21(mpynote:musicpy.note)->music21.note.Note:
+    """将musicpy音符转换为music21音符"""
     return music21.note.Note(str(mpynote).title().replace("b","-"),duration=music21.duration.Duration(mpynote.duration*4))
 
-def mpy_chord_to_m21(mpychord:musicpy.chord,start_time=0)->music21.stream.Stream:
+def mpy_chord_to_m21(mpychord:musicpy.chord)->music21.stream.Stream:
+    """将musicpy chord转换为music21 stream"""
     m21stream=music21.stream.Stream()
-    time=start_time
+    time=0
     for (note,interval) in zip(mpychord,mpychord.interval):
         m21stream.insert(time,mpy_note_to_m21(note))
         time+=interval*4
     return m21stream.chordify()
 
-def mpy_track_to_m21(mpytrack:musicpy.track)->music21.stream.Stream:
-    return mpy_chord_to_m21(mpytrack.content,start_time=mpytrack.start_time)
+def mpy_tempo_to_m21(mpytempo:musicpy.tempo)->music21.tempo.MetronomeMark:
+    """将musicpy曲速对象转换为music21曲速对象"""
+    return music21.tempo.MetronomeMark(number=mpytempo.bpm)
+
+def mpy_track_to_m21(mpytrack:musicpy.track)->music21.stream.Part:
+    """将musicpy track转换为music21 part"""
+    m21stream=music21.stream.Part()
+    mpychord=mpytrack.content
+    time=mpytrack.start_time
+    for (note,interval) in zip(mpychord,mpychord.interval):
+        m21stream.insert(time,mpy_note_to_m21(note))
+        time+=interval*4
+    return m21stream
+
+def mpy_piece_to_m21(mpypiece:musicpy.piece)->music21.stream.Score:
+    """将musicpy多轨工程转换为music21多轨工程"""
+    m21score=music21.stream.Score()
+    for tr in mpypiece:
+        m21part=mpy_track_to_m21(tr)
+        m21score.insert(0,m21part)
+    return m21score
+    #TODO:曲速
 
 def mpy_to_m21(mpyobj):
+    """将musicpy对象转换为music21对象"""
     return {
         musicpy.note:mpy_note_to_m21,
         musicpy.chord:mpy_chord_to_m21,
         musicpy.track:mpy_track_to_m21,
+        musicpy.piece:mpy_piece_to_m21,
+        musicpy.tempo:mpy_tempo_to_m21,
         }[type(mpyobj)](mpyobj)
